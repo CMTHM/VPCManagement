b8d9bb713988ab0d2f79ec68347c50287d750937
Chintamani Tripathy <chintamani19.gec@gmail.com>
2025-02-13 14:51:46 +0530

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,28 +2,37 @@ resource "aws_lambda_function" "create_vpc_lambda" {
   function_name = "CreateVPCFunction"
   runtime       = "python3.9"
   handler       = "index.lambda_handler"
-  role          = aws_iam_role.lambda_exec_role.arn
+  role          = aws_iam_role.lambda_execution_role.arn
 
   filename         = "lambda/create_vpc.zip"
-  source_code_hash = filebase64sha256("create_vpc.zip")
+  source_code_hash = filebase64sha256("lambda/create_vpc.zip")
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_policy_attach
+  ]
 }
 
 resource "aws_lambda_function" "get_vpc_lambda" {
   function_name = "GetVPCFunction"
   runtime       = "python3.9"
   handler       = "index.lambda_handler"
-  role          = aws_iam_role.lambda_exec_role.arn
+  role          = aws_iam_role.lambda_execution_role.arn
 
   filename         = "lambda/get_vpc.zip"
-  source_code_hash = filebase64sha256("get_vpc.zip")
+  source_code_hash = filebase64sha256("lambda/get_vpc.zip")
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_policy_attach
+  ]
 }
 
 resource "aws_lambda_function" "auth_lambda" {
   function_name = "VPCAuthFunction"
   runtime       = "python3.9"
   handler       = "index.lambda_handler"
-  role          = aws_iam_role.lambda_exec_role.arn
+  role          = aws_iam_role.lambda_execution_role.arn
 
   filename         = "lambda/auth_lambda.zip"
-  source_code_hash = filebase64sha256("auth.zip")
+  source_code_hash = filebase64sha256("lambda/auth_lambda.zip")
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_policy_attach
+  ]
 }
