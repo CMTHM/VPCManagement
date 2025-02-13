51d271c6f56ee98ba281ada140c4cabc24ffd134
unknown <chintamani19.gec@gmail.com>
2025-02-13 07:58:59 +0530

    updated iam

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,63 +1,55 @@
-resource "aws_iam_role" "lambda_exec_role" {
+resource "aws_iam_role" "lambda_execution_role" {
   name = "LambdaVPCExecutionRole"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
 resource "aws_iam_policy" "lambda_vpc_policy" {
   name        = "LambdaVPCPolicy"
-  description = "Policy for Lambda to manage VPC and write to DynamoDB"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "arn:aws:logs:*:*:*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ec2:CreateVpc",
-        "ec2:CreateSubnet",
-        "ec2:DescribeVpcs",
-        "ec2:DescribeSubnets"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "dynamodb:PutItem",
-        "dynamodb:GetItem"
-      ],
-      "Resource": "${aws_dynamodb_table.vpc_records.arn}"
-    }
-  ]
-}
-EOF
+  description = "IAM policy for Lambda to manage VPC and DynamoDB"
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:CreateVpc",
+          "ec2:CreateSubnet",
+          "ec2:DescribeVpcs",
+          "ec2:DescribeSubnets"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem"
+        ]
+        Resource = aws_dynamodb_table.vpc_records.arn
+      }
+    ]
+  })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_vpc_policy_attach" {
-  role       = aws_iam_role.lambda_exec_role.name
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_execution_role.name
   policy_arn = aws_iam_policy.lambda_vpc_policy.arn
 }
